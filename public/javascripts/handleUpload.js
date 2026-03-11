c59c9fc04ce059dff21848d0c95dd221c13b0a8e
Alex Paquibot <115098095+alexpqbt@users.noreply.github.com>
2026-03-11 15:38:03 +0800

    feat: selected files are able to be removed in the list

--- public/javascripts/handleUpload.js
+++ public/javascripts/handleUpload.js
@@ -1,12 +1,42 @@
 const fileInput = document.getElementById("file");
 const fileList = document.getElementById("file-list");
 
+let dt = new DataTransfer();
+
 fileInput.addEventListener("change", () => {
+  for (let file of fileInput.files) {
+    dt.items.add(file);
+  }
+
+  fileInput.files = dt.files;
+
+  renderList();
+});
+
+function renderList() {
   fileList.innerHTML = "";
 
-  Array.from(fileInput.files).forEach((file) => {
+  Array.from(dt.files).forEach((file, index) => {
     const li = document.createElement("li");
-    li.textContent = file.name;
+    li.innerHTML = `
+      ${file.name} 
+      <button type="button" class="remove-btn" onclick="removeFile(${index})">Remove</button>
+    `;
     fileList.appendChild(li);
   });
-});
+}
+
+window.removeFile = (index) => {
+  const newDt = new DataTransfer();
+
+  for (let i = 0; i < dt.files.length; i++) {
+    if (i !== index) {
+      newDt.items.add(dt.files[i]);
+    }
+  }
+
+  dt = newDt;
+  fileInput.files = dt.files;
+
+  renderList();
+};
